4a77a76fc8c97030aa00ea6e437a4d5793ddbc4a
Guille-coder <guillermoreyen.16@gmail.com>
2025-03-11 20:59:40 -0500

    index

---
+++ scripts/wayfinding.js
@@ -0,0 +1,10 @@
+// wayfinding.js
+// Function to highlight the current page
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
